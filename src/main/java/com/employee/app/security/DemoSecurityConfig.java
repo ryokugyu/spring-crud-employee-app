45b6293817c92fb32c5ce9590369c00e18172d2f
ryokugyu <himanshunailwal.rhce@gmail.com>
2023-10-25 19:48:49 +0530

    BCrypt password authentication added:

--- src/main/java/com/employee/app/security/DemoSecurityConfig.java
+++ src/main/java/com/employee/app/security/DemoSecurityConfig.java
@@ -1,20 +1,49 @@
 package com.employee.app.security;
 
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation .Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+// adding JDBC support, no more hardcoded value
     @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employee/getEmployeeDetails").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employee/getEmployeeDetailsById/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employee/addEmployee").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.POST, "/api/employee/updateEmployeeDetails").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employee/deleteEmployeeDetails/**").hasRole("ADMIN")
+        );
+
+        // use http basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+
+        //disable csrf
+        http.csrf(csrf -> csrf.disable());
+
+
+        return  http.build();
+    }
+
+    /*   @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
 
         UserDetails john = User.builder()
@@ -38,27 +67,5 @@ public class DemoSecurityConfig {
         return new InMemoryUserDetailsManager(john,mary,susan);
     }
 
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http.authorizeHttpRequests(configurer ->
-                configurer
-                        .requestMatchers(HttpMethod.GET, "/api/employee/getEmployeeDetails").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "/api/employee/getEmployeeDetailsById/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.POST, "/api/employee/addEmployee").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.POST, "/api/employee/updateEmployeeDetails").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "/api/employee/deleteEmployeeDetails/**").hasRole("ADMIN")
-        );
-
-        // use http basic authentication
-        http.httpBasic(Customizer.withDefaults());
-
-
-        //disable csrf
-        http.csrf(csrf -> csrf.disable());
-
-
-        return  http.build();
-    }
+*/
 }
